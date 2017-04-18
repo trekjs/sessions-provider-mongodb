121c2486cf8c9e8a03570da8f3adb9d2e52fa910
Fangdun Cai <cfddream@gmail.com>
2017-04-18 21:54:00 +0800

    init

--- index.js
+++ index.js
@@ -1,20 +1,82 @@
+/*!
+ * sessions-provider-mongodb
+ * Copyright(c) 2017 Fangdun Cai <cfddream@gmail.com> (https://fundon.me)
+ * MIT Licensed
+ */
+
 'use strict'
 
-const mongodb = require('mongodb')
+const { MongoClient } = require('mongodb')
 
-const defaults = {}
+const defaults = {
+  // Mongodb server uri
+  uri: 'mongodb://localhost:27017/test',
+  // Mongodb collection name
+  collectionName: 'sessions',
+  // Mongodb client options
+  options: undefined
+}
+
+const DB = Symbol('MongodbProvider#db')
 
 module.exports = class MongodbProvider {
 
-  constructor (options) {
+  constructor (options = {}) {
+    Object.keys(options).forEach(k => {
+      if (!(k in defaults)) delete options[k]
+    })
+    Object.assign(this, defaults, options)
   }
 
-  async clear () {}
+  get client () {
+    return MongoClient.connect(this.uri, this.options)
+  }
 
-  async get (sid) {}
+  get db () {
+    if (!this[DB]) {
+      return this.client.then(db => {
+        this[DB] = db
+        return db
+          .collection(this.collectionName)
+          .ensureIndex({ expires: 1 }, { expireAfterSeconds: 3600 })
+          .then(() => db)
+      })
+    }
+    return Promise.resolve(this[DB])
+  }
 
-  async set (ssid, sess, expires) {}
+  get collection () {
+    return this.db.then(db => db.collection(this.collectionName))
+  }
 
-  async delete (sid) {}
+  async clear () {
+    return (await this.collection).remove({})
+  }
+
+  async get (sid) {
+    return (await this.collection).findOne({ _id: sid }).then(d => {
+      return (d && d.session)
+    })
+  }
+
+  async has (sid) {
+    return Boolean(await this.get(sid))
+  }
+
+  async set (sid, session, expires = 0) {
+    return (await this.collection).update(
+      { _id: sid },
+      { session, expires: new Date(Date.now() + expires) },
+      { upsert: true }
+    )
+  }
+
+  async delete (sid) {
+    return (await this.collection).remove({ _id: sid })
+  }
+
+  quit () {
+    return this.db.then(db => db.close())
+  }
 
 }
---
+++ test.js
@@ -0,0 +1,131 @@
+import test from 'ava'
+import MongodbProvider from './'
+
+function sleep (ttl = 1000) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ttl)
+  })
+}
+
+test('should create a mongodb client', t => {
+  const provider = new MongodbProvider({
+    customKey: 233
+  })
+
+  t.not(provider.client, undefined)
+
+  provider.quit().then(t.context.done)
+})
+
+test('should return undefined', async t => {
+  const provider = new MongodbProvider()
+
+  const sess = await provider.get('233')
+
+  t.is(sess, null)
+
+  await provider.quit()
+})
+
+test('should save a session', async t => {
+  const provider = new MongodbProvider()
+
+  await provider.set('233', {
+    cookie: {}
+  }, 2000)
+
+  const sess = await provider.get('233')
+
+  t.deepEqual(sess, {
+    cookie: {}
+  })
+
+  await provider.quit()
+})
+
+test('should delete a session', async t => {
+  const provider = new MongodbProvider()
+
+  await provider.set('233', {
+    cookie: {}
+  }, 2000)
+
+  let sess = await provider.get('233')
+
+  t.deepEqual(sess, {
+    cookie: {}
+  })
+
+  await provider.delete('233')
+
+  sess = await provider.get('233')
+
+  t.is(sess, null)
+
+  await provider.quit()
+})
+
+test('should clear all sessions', async t => {
+  const provider = new MongodbProvider()
+
+  await provider.set('233', {
+    cookie: {}
+  }, 2000)
+
+  await provider.set('377', {
+    cookie: {}
+  }, 2000)
+
+  await provider.clear('233')
+
+  const sess0 = await provider.get('233')
+  const sess1 = await provider.get('377')
+
+  t.is(sess0, null)
+  t.is(sess1, null)
+
+  await provider.quit()
+})
+
+test('should throw error when mongodb is already closed', async t => {
+  const provider = new MongodbProvider()
+
+  await provider.set('610', 'trek engine', 2000)
+
+  const has = await provider.has('610')
+
+  t.is(has, true)
+
+  await provider.quit()
+
+  const error = await t.throws(provider.get('610'))
+
+  t.true(/opology was destroyed/.test(error.message))
+})
+
+test('should automatically delete a session', async t => {
+  const provider = new MongodbProvider({
+    collectionName: 'sessions2'
+  })
+
+  await provider.set('1024', {
+    cookie: {}
+  }, 1000)
+
+  await sleep(500)
+
+  const sess = await provider.get('1024')
+
+  t.deepEqual(sess, {
+    cookie: {}
+  })
+
+  await sleep(4500)
+
+  const sess2 = await provider.get('1024')
+
+  // See: https://docs.mongodb.com/manual/tutorial/expire-data/
+  t.is(sess2, null)
+
+  await provider.quit()
+})
